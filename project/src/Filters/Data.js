361a4975092116db74602b8d55f7e0732292dec0
Charme-Lee <charmelishivha2@gmail.com>
2025-11-11 15:07:11 +0200

    add GenreFilter component using PodcastContext for genre filtering

--- project/src/Filters/Data.js
+++ project/src/Filters/Data.js
@@ -1,3 +1,5 @@
+//Re-export several components from their individual files,
+// so that you can import them as import { SearchBar, SortSelect, GenreFilter } from "./filters";
 export { default as SearchBar } from "./SearchBar";
 export { default as SortSelect } from "./SortSelect";
 export { default as GenreFilter } from "./GenreFilter";
