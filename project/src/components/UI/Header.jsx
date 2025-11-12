0de46ae7ca43090f583d800294869c7a5f746431
Charme-Lee <charmelishivha2@gmail.com>
2025-11-12 11:01:21 +0200

    Add header content and css styling

---
+++ project/src/components/UI/Header.jsx
@@ -0,0 +1,24 @@
+import styles from "./Header.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+
+export default function Header() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <header className={styles.appHeader}>
+      <div>
+        <Link to="/">🎙️ Podcast App</Link>
+      </div>
+      <div>
+        <Link to="/favourites" className={styles.favouritesLink}>
+          Favourites
+        </Link>
+      </div>
+      <button onClick={toggleTheme} className={styles.themeToggle}>
+        {theme === "dark" ? "🌞" : "🌚"}
+      </button>
+    </header>
+  );
+}
